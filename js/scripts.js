ae20fdbc483881a2ce1fc339f61fbdf1a5a6b32f
Kevin Finley and Levi Bibo <kevin+levi@>
2016-10-19 16:12:46 -0700

    add style and jquery to html

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".orderForm .col-md-12 form").submit(function(event) {
+    $(".name").text($("#name").val());
+    $(".address").text($("#address").val());
+    $(".cpu").text($("input:radio[name=cpu]:checked").val());
+    $(".gpu").text($("input:radio[name=gpu]:checked").val());
+
+    $(".receipt").slideDown();
+    $(".orderForm").slideUp();
+
+    event.preventDefault();
+  });
+});
